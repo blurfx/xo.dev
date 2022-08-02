cd7ceaa18be3f971ae5f088cd379b5e92ffdb057
blurfx <iam@xo.dev>
2022-08-02 13:10:18 +0900

    chore: update status

--- blog-config.ts
+++ blog-config.ts
@@ -1,7 +1,7 @@
 const config = {
   title: '~/xo.dev',     // Blog name
   author: 'Changhui Lee', // Your name
-  description: '데브시스터즈에서 소프트웨어 엔지니어로 일하고 있습니다. 분야에 상관없이 소프트웨어를 개발하는 일을 사랑하며, 일을 제대로 잘하는 것에 관심이 많습니다.',
+  description: '분야에 상관없이 소프트웨어를 개발하는 일을 사랑하며, 일을 제대로 잘하는 것에 관심이 많습니다.',
   siteUrl: 'https://xo.dev',
   thumbnail: '/thumbnails/main.png',  // Default blog thumbnail. i.e. '/thumbnails/main.png'
   social: {
